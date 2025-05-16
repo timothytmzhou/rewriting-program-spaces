fa6d39ec64fbcf49bcf30a7f82c4fb2607e6a40f
timothytmzhou <timothytmzhou@gmail.com>
2025-05-16 11:48:17 -0700

    testing infrastructure

---
+++ tests/utils.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from networkx import DiGraph, is_isomorphic
+from core.rewrite import *
+
+
+def reset(test_function):
+    """
+    Decorator for tests which clears the state of the TRS before running the test.
+    """
+    @wraps(test_function)
+    def wrapper(*args, **kwargs):
+        rewriter.clear()
+        test_function(*args, **kwargs)
+    return wrapper
+
+
+def dependencies_isomorphic_to(edges):
+    """
+    Check if the dependencies graph is isomorphic to the given edges.
+    """
+    return is_isomorphic(rewriter.dependencies, DiGraph(edges))
